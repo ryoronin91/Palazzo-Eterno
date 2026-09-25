f6d1b11d9dc809111579d332546c3c954952294c
ryoronin91 <149951443+ryoronin91@users.noreply.github.com>
2026-09-25 16:26:56 +0200

    afsa

--- js/combat-ai.js
+++ js/combat-ai.js
@@ -651,36 +651,7 @@ async function runEnemyAI(
 
     }
 
-    const monsterType =
-    String(
-        currentEntity.monster_type ||
-        ""
-    ).toLowerCase();
-
-
-if (
-    monsterType ===
-    "goblin_sputafuoco"
-) {
-
-    console.log(
-        "IA: GOBLIN SPUTAFUOCO"
-    );
-
-}
-
-
-if (
-    monsterType ===
-    "goblin_sciamano"
-) {
-
-    console.log(
-        "IA: GOBLIN SCIAMANO"
-    );
-
-}
-
+    
     const aiTurnKey =
         `${combatSession.round_number}:${currentEntity.id}`;
 
@@ -695,6 +666,35 @@ if (
 
     }
 
+        const monsterType =
+        String(
+            currentEntity.monster_type ||
+            ""
+        ).toLowerCase();
+
+
+    if (
+        monsterType ===
+        "goblin_sputafuoco"
+    ) {
+
+        console.log(
+            "IA: GOBLIN SPUTAFUOCO"
+        );
+
+    }
+
+
+    if (
+        monsterType ===
+        "goblin_sciamano"
+    ) {
+
+        console.log(
+            "IA: GOBLIN SCIAMANO"
+        );
+
+    }
 
     enemyAITurnKey =
         aiTurnKey;
